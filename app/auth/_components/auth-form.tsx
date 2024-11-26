d4c31cd6a0d2001aae0074750e6f04568c351b53
Kaic Malta <51099093+MaltaHub@users.noreply.github.com>
2024-11-26 02:12:25 +0000

    Página Auth manda email como parâmetro!

--- app/auth/_components/auth-form.tsx
+++ app/auth/_components/auth-form.tsx
@@ -1,5 +1,26 @@
+'use client'
+
+import { useForm } from "react-hook-form"
+
 export function AuthForm() {
+    const form = useForm()
+
+    const handleSubmit = form.handleSubmit((data) => {
+        console.log(data)
+    })
+
     return (
-        <h1>Tela de Login</h1>
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                Página de Login
+            </main>
+            <form onSubmit={handleSubmit} className="space-y-4">
+                <div className="space-y-2">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" placeholder="email@example.com" required type="email" {... form.register('email')}/>
+                </div>
+                <button className="w-full" type="submit">Send Magic Link</button>
+            </form>
+        </div>
     )
 }
