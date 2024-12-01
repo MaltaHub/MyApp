8507084f71e37cba603ac2ba977c817dce289754
Kaic Malta <51099093+MaltaHub@users.noreply.github.com>
2024-12-01 21:11:45 +0000

    Adicionadas rotas de redirecionamento ao Enviar Magic Link! Trocada a página home por dashboard - Página de conexão!
    O Nodemailer também foi adicionado na versão anterior!
    
    (TESTE DE NOVA FUNÇÂO EM AUTH-FORM)

--- app/auth/_components/auth-form.tsx
+++ app/auth/_components/auth-form.tsx
@@ -1,15 +1,42 @@
 'use client'
 
 import { signIn } from "next-auth/react"
+import { resolve } from "path"
 import { useForm } from "react-hook-form"
+import toast, { Toaster } from "react-hot-toast"
 
 export function AuthForm() {
     const form = useForm()
 
     const handleSubmit = form.handleSubmit(async (data) => {
-        console.log(data)
+        //let notifier //seria o objeto resultado de toast.success mas aparentemente não funciona
 
-        await signIn('email', {email: data.email})
+        try {
+            console.log(data)
+            await signIn('email', { email: data.email, redirect: false })
+
+            //cria a notificação de sucesso sem duração
+            toast.success('Check your email for the magic link to login', { id: 'sucess', duration: 8000, position: 'bottom-center' });
+
+            //define a função de espera síncrona com async/await utilizando Promise
+            const functionWait = (duration: number) => new Promise((resolve) => setTimeout(() => {
+                return console.log("Esperando",duration,"segundos!")
+            }, duration | 1000))
+
+            //função de espera e exclusão de notificação
+            async function setWait(duration: number) {
+                console.log("Começando espera!")
+                await functionWait(duration)
+
+                //apaga a notificação
+                toast.dismiss('sucess')
+                console.log("Notificação desligada!")
+            }
+
+            setWait(8000)
+        } catch (error) {
+            toast.error('An error ocurred. Please try again.')
+        }
     })
 
     return (
@@ -20,7 +47,7 @@ export function AuthForm() {
             <form onSubmit={handleSubmit} className="space-y-4">
                 <div className="space-y-2">
                     <label htmlFor="email">Email</label>
-                    <input id="email" placeholder="email@example.com" required type="email" {... form.register('email')}/>
+                    <input id="email" placeholder="email@example.com" required type="email" {...form.register('email')} />
                 </div>
                 <button className="w-full" type="submit">Send Magic Link</button>
             </form>
--- app/services/auth/index.ts
+++ app/services/auth/index.ts
@@ -12,10 +12,26 @@ export const {
     // fique atento para quaisquer problemas que venham a
     // aparecer, pois estamos utilizando o auth-beta@latest
     adapter: PrismaAdapter(prisma),
+
+    pages: {
+        signIn: '/auth',
+        signOut: '/auth',
+        error: '/auth',
+        verifyRequest: '/auth',
+        newUser: '/dashboard'
+    },
+
+    callbacks: {
+        async signIn(params) {
+            
+        },
+    },
+
     providers: [EmailProvider({
         //Aqui pegamos as variáveis de ambiente
         server: process.env.EMAIL_SERVER,
         from: process.env.EMAIL_FROM
     })],
-    secret: process.env.NEXTAUTH_SECRET, // Adicione esta linha
+
+    secret: process.env.NEXTAUTH_SECRET, // Chave Secreta em ambiente
 })
